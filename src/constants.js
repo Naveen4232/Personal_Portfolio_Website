6dae52136c4c6aab881d462f73f7252d18e9e75b
Naveen E <144200750+Naveen4232@users.noreply.github.com>
2024-08-28 11:20:57 +0530

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,39 @@
+
+export const services = [
+    {
+        title: "Technology Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Software Engineer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Filmmaker",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Naveen';
+
+export const experiences = [
+    {
+        'company': 'Cognizant Technology Solutions',
+        'role': 'Java Intern (backend)',
+        'duration': 'May 2024 - July 2024',
+        'logo': 'https://upload.wikimedia.org/wikipedia/commons/5/5a/Logo_Cognizant.png',
+        'points': [
+            'Gained experience with Git version control contributing to the development and maintenance of code repositories.',
+            'Worked on the application dashboard, making it more user-friendly and improving its overall functionality📱.',
+            'Gained experience with Java, SQL, CSS, HTML, and JavaScript Basics.',
+            'I have learned new skills and technologies and taken courses in udemy.'
+        ],
+        'url': 'https://www.cognizant.com/in/en',
+    },
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = process.env.REACT_APP_EMAIL_JS_SERVICE_ID;
+export const EMAIL_JS_TEMPLATE_ID = process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID;
+export const EMAIL_JS_PUBLIC_KEY = process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY;
